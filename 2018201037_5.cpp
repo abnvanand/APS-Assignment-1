a8b61b9f34b7a825c6ae68a8182f34c534047246
Abhinav Anand <abhinav_anand@live.com>
2018-09-04 23:52:33 +0530

    Add solution of question 5

---
+++ 2018201037_5.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <vector>
+
+#define PARENT(i) (i-1)/2
+#define LEFT_CHILD(i) (2 * i) + 1
+#define RIGHT_CHILD(i) (2 * i) + 2
+
+#define MIN_HEAP 1
+#define MAX_HEAP 2
+
+using namespace std;
+
+/**
+ * A min heap implementation
+ * if index of parent == i
+ * the two children are at 2i+1 and 2i+2
+ *
+ * if index of child = j
+ * then parent is (j-1)/2
+ */
+class heap {
+    vector<int> arr;
+    int type = MIN_HEAP;
+
+public:
+    heap() = default;
+
+    heap(int type) {
+        this->type = type;
+    }
+
+    unsigned long size() {
+        return arr.size();
+    }
+
+    void swap(int &a, int &b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    int get_heap_aware_number(int x) {
+        if (type == MAX_HEAP)
+            return -x;
+        else
+            return x;
+    }
+
+    void heapify_bottom() {
+        int i = static_cast<int>(this->size() - 1);
+
+        while (i > 0) {
+            if (arr[i] < arr[PARENT(i)]) {
+                swap(arr[i], arr[PARENT(i)]);
+                i = PARENT(i);
+            } else
+                break;
+        }
+    }
+
+    void heapify_top() {
+        if (this->size() <= 1)
+            return;
+
+        int i = 0;
+        // start from root
+        while (i < this->size()) {
+            if (RIGHT_CHILD(i) >= this->size()) {
+                // right child does not exist
+                // test with left child only
+                if (LEFT_CHILD(i) >= this->size())
+                    return;
+
+                // only left child exists
+                if (arr[i] > arr[LEFT_CHILD(i)])
+                    swap(arr[i], arr[LEFT_CHILD(i)]);
+                return;
+            }
+            // both children exist
+
+
+            if (arr[i] > arr[LEFT_CHILD(i)] || arr[i] > arr[RIGHT_CHILD(i)]) {
+                // if parent is greater than any of its child,
+                // then swap the minimum to the top
+                if (arr[LEFT_CHILD(i)] < arr[RIGHT_CHILD(i)]) {
+                    // swap left with parent
+                    swap(arr[i], arr[LEFT_CHILD(i)]);
+                    i = LEFT_CHILD(i);
+                } else {
+                    // swap right with parent
+                    swap(arr[i], arr[RIGHT_CHILD(i)]);
+                    i = RIGHT_CHILD(i);
+                }
+            } else {
+                // root is already smaller than both its children
+                break;
+            }
+        }
+    }
+
+    bool empty() {
+        return arr.empty();
+    }
+
+    int top() {
+        return get_heap_aware_number(arr[0]);
+    }
+
+    void pop() {
+        arr[0] = arr[arr.size() - 1];
+        arr.pop_back(); // decrease size of underlying ds
+
+        heapify_top();
+    }
+
+    void push(int x) {
+        arr.push_back(get_heap_aware_number(x));
+        heapify_bottom();
+    }
+};
+
+
+int main() {
+    // Contains smaller numbers
+//    priority_queue<int, vector<int>> maxheap;
+    heap maxheap = heap(MAX_HEAP);
+    // Contains larger numbers
+//    priority_queue<int, vector<int>, greater<>> minheap;
+    heap minheap = heap(MIN_HEAP);
+
+    int n, x;
+    cin >> n;
+    while (n--) {
+        cin >> x;
+
+        if (minheap.empty() || minheap.top() < x)
+            minheap.push(x);
+        else
+            maxheap.push(x);
+
+        // If the difference b/w the sizes is more than one
+        // then pop() from the bigger one and add to smaller one
+        if (minheap.size() > maxheap.size() + 1) {
+            maxheap.push(minheap.top());
+            minheap.pop();
+        } else if (maxheap.size() > minheap.size() + 1) {
+            minheap.push(maxheap.top());
+            maxheap.pop();
+        }
+
+        if (minheap.size() == maxheap.size()) {
+            printf("%.1f\n", (minheap.top() + maxheap.top()) / 2.0);
+        } else {
+            printf("%.1f\n", 1.0 * (minheap.size() > maxheap.size() ? minheap.top() : maxheap.top()));
+        }
+    }
+    return 0;
+}
